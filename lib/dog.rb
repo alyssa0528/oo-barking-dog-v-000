1697b7f1572addae61db167c82c3b783d681a508
Alyssa Kim <alyssa.kim@gmail.com>
2018-01-31 16:20:45 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,7 +1,8 @@
 # Your code goes here!
 class Dog 
-  def initialize(name) #initializer 
+  def initialize(name, bark) #initializer 
     @name = name
+    @bark = bark
   end
   def name=(name) #getter
     @name = name
